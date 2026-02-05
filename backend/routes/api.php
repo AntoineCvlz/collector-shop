7c7226427af1f46cb7755c28eec0581e2c5602c0
unknown <antoine.cuvilliez@outlook.fr>
2026-02-05 14:24:58 +0100

    [backend] feat(hello-world)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello-world', function () {
+    return response()->json([
+        'message' => 'Hello World'
+    ]);
+});
+
+
 Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
     // --------------- Register and Login ----------------//
     Route::post('register', 'AuthenticationController@register')->name('register');
